fdf025d8b8e2c47813aeadb4af1a785d4ed20c4a
Pibo Shongwe <piboman@yahoo.com>
2023-07-13 11:53:27 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
 #include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
 
 int main(void)
 {
-	const char *message;
+	const char *message = "and that piece of art is useful
+\" - Dora Korpar, 2015-10-19\n";
 
-message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, strlen(message));
-	return (0);
+	write(STDOUT_FILENO, message, 59);
+	return (1);
 }
-
