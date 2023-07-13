4d55621bc812ef880608e34844babf4b03a8accf
Pibo Shongwe <piboman@yahoo.com>
2023-07-13 11:57:43 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, "
-                     "2015-10-19\n";
+"2015-10-19\n";
 
 	write(STDOUT_FILENO, message, 59);
 	return (1);
