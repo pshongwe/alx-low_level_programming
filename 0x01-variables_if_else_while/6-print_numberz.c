d4f63f6c67ae78520910698df9607b21b3824060
Pibo Shongwe <piboman@yahoo.com>
2023-07-14 04:21:51 -0700

    print numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(48 + i);
+	}
+	putchar('\n');
+	return (0);
+}
