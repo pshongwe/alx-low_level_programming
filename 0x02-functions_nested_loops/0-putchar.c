11dda2df5415ae9505bc827c8906e0777118c21f
Pibo Shongwe <piboman@yahoo.com>
2023-07-18 02:43:42 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,6 +8,14 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char str[8] = "_putchar";
+	int i;
+	int n = (unsigned int)(sizeof(str) / sizeof(str[0]));
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
