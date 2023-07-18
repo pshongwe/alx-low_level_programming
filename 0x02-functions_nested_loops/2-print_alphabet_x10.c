df955fa1cde78cf63a088b51684b7658af842e9c
Pibo Shongwe <piboman@yahoo.com>
2023-07-18 03:20:29 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet 10x
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();		
+	}
+}
