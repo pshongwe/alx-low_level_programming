642452295832d03bcd21b29b1c7cf4174e6a9e42
Pibo Shongwe <piboman@yahoo.com>
2023-07-18 14:28:59 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,11 +13,9 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d", n);
 			if (n != 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", n);
 			}
 			n++;
 		}
@@ -26,11 +24,9 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d", n);
 			if (n != 98)
 			{
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", n);
 			}
 			n--;
 		}
