c976b62ba8814d10c1093362bdb3bccc8e21c2a2
Pibo Shongwe <piboman@yahoo.com>
2023-07-20 02:17:45 -0700

    update

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,7 +7,7 @@
  * @n: input int
  * Return: void
  */
-int positive_or_negative(int n)
+void positive_or_negative(int n)
 {
 	if (n > 0)
 	{
@@ -21,5 +21,4 @@ int positive_or_negative(int n)
 	{
 		printf("%d is zero\n", n);
 	}
-	return (0);
 }
