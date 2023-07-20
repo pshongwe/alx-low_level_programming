2b6e9b3149910c845d22a6120e9fe80876d25b08
Pibo Shongwe <piboman@yahoo.com>
2023-07-20 01:37:42 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,21 +5,31 @@
  *
  * Return: Alway 0 (Success)
  */
-int main(void) {
-	int count = 2;
-	long fib1 = 1;
-	long fib2 = 2;
-	long fib;
+int main(void)
+{
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	printf("%lu, %lu", fib1, fib2);
-	while (count < 98)
+	j = 1;
+	k = 2;
+	printf("%lu", j);
+	for (i = 1; i < 91; i++)
 	{
-		fib = fib1 + fib2;
-
-		printf(", %lu", fib);
-		fib1 = fib2;
-		fib2 = fib;
-		count++;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
+	}
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
 	}
 	printf("\n");
 	return (0);
