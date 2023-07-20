e7f0142ed42b9ece35f6dcf98c105595d5e2ced6
Pibo Shongwe <piboman@yahoo.com>
2023-07-19 20:12:14 -0700

    update

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Returns: void
+ */
+void main(void)
+{
+	int i;
+	int result = 0;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			result += i;
+		}
+	}
+	printf("%d\n", result); 
+}
+
