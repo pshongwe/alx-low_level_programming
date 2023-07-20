7877dd05f6586b7878fdec6889c8278db51d0764
Pibo Shongwe <piboman@yahoo.com>
2023-07-20 00:54:57 -0700

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,28 +3,30 @@
 /**
  * main - entry point
  *
- * Returns: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int i;
-	unsigned long long int a = 1;
-	unsigned long long int b = 2;
-	unsigned long long int result = 0;
+	long a = 1;
+	long b = 2;
+	long result = 0;
 
+	printf("%lu, %lu", a, b);
 	for (i = 3; i < 50; i++)
 	{
 		result = a + b;
 		if ( i != 49)
 		{
-			printf(", %lld", result);
+			printf(", %lu", result);
 		}
 		else
 		{
-			printf("%lld\n", result);
+			printf("%lu\n", result);
 		}
 		a = b;
 		b = result;
 	}
+	return (0);
 }
 
