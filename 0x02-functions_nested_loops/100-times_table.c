9da57609d916fd8f27bea0b3bfe336e84f75edb4
Pibo Shongwe <piboman@yahoo.com>
2023-07-19 19:57:50 -0700

    update

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - times table
+ * @n: input number
+ * Returns: void
+ */
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	if (n > 15 || n < 0)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+			if (j == 0)
+			{
+				_putchar('0' + result);
+			}
+			else if (result < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + result);
+			}
+			else if (result >= 10 && result < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + (result / 10));
+				_putchar('0' + (result % 10));
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((result / 100) + '0');
+				_putchar(((result / 10) % 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
