47331ccbc9c2a866e226b70938615832e85e88d7
Pibo Shongwe <piboman@yahoo.com>
2023-07-20 17:17:27 -0700

    u

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle using #
+ * @size: input size
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		i = 1;
+		while (i <= size)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+                        {
+                                _putchar('#');
+                        }
+			i++;
+			_putchar('\n');
+		}
+	}
+}
