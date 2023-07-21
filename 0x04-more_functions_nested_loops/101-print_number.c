62af367376bfc7d5899ee27aa21988274c546a9b
Pibo Shongwe <piboman@yahoo.com>
2023-07-20 17:53:29 -0700

    u

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,14 +7,16 @@
  */
 void print_number(int n)
 {
+	unsigned int m = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		m = -m;
 	}
-	if (n / 10)
+	if ((n / 10) > 0)
 	{
-		print_number(n / 10);
+		print_number(m / 10);
 	}
 	_putchar('0' + (n % 10));
 }
