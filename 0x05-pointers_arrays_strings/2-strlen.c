8fa4e3da62951875a2727c03b6f0ef1cbf57f58a
Pibo Shongwe <piboman@yahoo.com>
2023-07-24 14:45:20 -0700

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,19 @@
 /**
+ * stringLength - calculates length of string
+ * @s: input string
+ * Return: length of string
+ */
+int stringLength(const char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length] != '\0')
+		length++;
+	return (length);
+}
+
+/**
  * _strlen - calculates length of a string
  * @s: pointer to string
  *
@@ -6,5 +21,5 @@
  */
 int _strlen(char *s)
 {
-	return (sizeof(s) / sizeof(char));
+	return (stringLength(s));
 }
