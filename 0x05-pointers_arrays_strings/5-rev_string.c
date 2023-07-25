97785ca8e7f0d76f38d81f815e27fd062cccc4a0
Pibo Shongwe <piboman@yahoo.com>
2023-07-24 20:31:23 -0700

    update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: input string
+ */
+
+void rev_string(char *s)
+{
+	char j, temp;
+	int i, k, len;
+
+	j = '\0';
+	i = 0;
+	len = 0;
+
+	while (s[i] != j)
+		i++;
+	len = i - 1;
+	for (k = 0; k < i / 2; k++)
+	{
+		temp = s[k];
+		s[k] = s[len];
+		s[len--] = temp;
+	}
+}
