d36dd3193382ea49cdc01cbb79176db396387a44
Pibo Shongwe <piboman@yahoo.com>
2023-07-24 20:33:54 -0700

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - that prints half of a string
+ * @str: input string
+ */
+
+void puts_half(char *str)
+{
+	int i, j, len, n;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	len = i;
+
+	if ((len % 2) != 0)
+	{
+		n = (len - 1) / 2;
+		for (j = n + 1; j < len; j++)
+			_putchar(str[j]);
+	}
+	else
+	{
+		n = (len / 2);
+		for (j = n; str[j] != '\0'; j++)
+			_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
