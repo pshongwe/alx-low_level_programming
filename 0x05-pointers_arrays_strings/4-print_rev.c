fe765e250ba4e09e7512775b2394417266d61158
Pibo Shongwe <piboman@yahoo.com>
2023-07-24 20:27:42 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	char j;
+	int i, k;
+
+	j = '\0';
+	i = 0;
+
+	while (s[i] != j)
+		i++;
+	for (k = i - 1; k >= 0; k--)
+	{
+		_putchar(s[k]);
+	}
+	_putchar('\n');
+}
