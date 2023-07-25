24c62b98d6bcf7445993f91fdda08c8a3828c074
Pibo Shongwe <piboman@yahoo.com>
2023-07-24 20:25:21 -0700

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: input string
+ */
+
+void puts2(char *str)
+{
+	int count;
+
+	count = 1;
+	if (str[0] != '\0')
+	{
+		_putchar(str[0]);
+		while (str[count] != '\0')
+		{
+			if (count % 2 == 0)
+				_putchar(str[count]);
+			count++;
+		}
+	}
+	_putchar('\n');
+}
