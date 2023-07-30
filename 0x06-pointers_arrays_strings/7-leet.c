fdd0320e4dbdce5b42a12203d4b609d784c4e8f5
Pibo Shongwe <piboman@yahoo.com>
2023-07-30 11:58:46 -0700

    update

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+/**
+ * leet - leet replacement
+ * @str: input string
+ * Return: result string
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char *leetChars;
+	char *leetReplacements;
+
+	*leetChars = "AEOTL";
+	*leetReplacements = "43071";
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leetChars[j] != '\0'; j++)
+		{
+			if (str[i] == leetChars[j] || str[i] == leetChars[j] + 32)
+			{
+				str[i] = leetReplacements[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
+
