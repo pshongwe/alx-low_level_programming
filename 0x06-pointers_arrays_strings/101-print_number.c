3ebff24cf75c184e9d6457bc01ec712bc272943f
Pibo Shongwe <piboman@yahoo.com>
2023-08-02 14:16:43 -0700

    update

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,7 @@
 #include "main.h"
-
 /**
  * print_number - prints a number
  * @n: number to print
- * Return: nothing
  */
 void print_number(int n)
 {
@@ -16,37 +14,31 @@ void print_number(int n)
 		}
 		_putchar(n + '0');
 	}
-	else if ((n >= 10 && n <= 99) || (n <= -10 && n >= -99))
+	else
 	{
 		if (n < 0)
 		{
 			_putchar(45);
 			n = n * -1;
 		}
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 100 && n <= 999) || (n <= -100 && n >= -999))
-	{
-		if (n < 0)
+
+		if ((n >= 10 && n <= 99) || (n <= -10 && n >= -99))
 		{
-			_putchar(45);
-			n = n * -1;
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
-		_putchar((n / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 1000 && n <= 9999) || (n <= -1000 && n >= -9999))
-	{
-		if (n < 0)
+		else if ((n >= 100 && n <= 999) || (n <= -100 && n >= -999))
 		{
-			_putchar(45);
-			n = n * -1;
+			_putchar((n / 100) + '0');
+			_putchar(((n % 100) / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
-		_putchar((n / 1000) + '0');
-		_putchar(((n % 1000) / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar((n % 10) + '0');
+		else if ((n >= 1000 && n <= 9999) || (n <= -1000 && n >= -9999))
+		{
+			_putchar((n / 1000) + '0');
+			_putchar(((n % 1000) / 100) + '0');
+			_putchar(((n % 100) / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
 	}
 }
