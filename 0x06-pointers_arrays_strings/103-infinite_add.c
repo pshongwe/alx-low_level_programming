b20129028e9eab7a85bd0fd7247cd49a58fc662d
Pibo Shongwe <piboman@yahoo.com>
2023-08-03 18:05:54 -0700

    update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -6,19 +6,28 @@
  * @size_r: The size of the buffer.
  * Return: A pointer to the result (r) or 0 if the result cannot be stored.
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
 int len1 = 0, len2 = 0, carry = 0, sum, i = 0;
-while (n1[len1]) len1++;
-while (n2[len2]) len2++;
-if (len1 >= size_r || len2 >= size_r) return (0);
-for (; len1 > 0 || len2 > 0 || carry; len1--, len2--, i++) {
-sum = carry + (len1 > 0 ? n1[len1 - 1] - '0' : 0) + (len2 > 0 ? n2[len2 - 1] - '0' : 0);
+while (n1[len1])
+len1++;
+while (n2[len2])
+len2++;
+if (len1 >= size_r || len2 >= size_r)
+return (0);
+for (; len1 > 0 || len2 > 0 || carry; len1--, len2--, i++)
+{
+sum = carry + (len1 > 0 ?
+n1[len1 - 1] - '0' : 0) + (len2 > 0 ?
+n2[len2 - 1] - '0' : 0);
 carry = sum / 10;
 r[i] = (sum % 10) + '0';
 }
-if (i >= size_r) return (0);
+if (i >= size_r)
+return (0);
 r[i] = '\0';
-for (int j = 0, k = i - 1; j < k; j++, k--) {
+for (int j = 0, k = i - 1; j < k; j++, k--)
+{
 char tmp = r[j];
 r[j] = r[k];
 r[k] = tmp;
