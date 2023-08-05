57e97da702211110814c5bfe7b2374feff89d1f1
Pibo Shongwe <piboman@yahoo.com>
2023-08-04 20:23:04 -0700

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: length of arg array
+ * @argv: arg array
+ *Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n %d", argv[0], argc);
+return (0);
+}
+
