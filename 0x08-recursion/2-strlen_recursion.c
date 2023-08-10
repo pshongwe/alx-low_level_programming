55bbfec38602740b28bc38e32caaf051603c5ccc
Pibo Shongwe <piboman@yahoo.com>
2023-08-10 12:12:26 -0700

    u

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,7 +4,7 @@
  * _strlen_recursion - returns the lenght of a string
  * @s: string parameter
  *
- * Return: lenght of the string s
+ * Return: length of the string s
  */
 int _strlen_recursion(char *s)
 {
