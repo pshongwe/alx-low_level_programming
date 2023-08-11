3024a512aa57a6e756b3b5c832382daa8a9cd2cf
Pibo Shongwe <piboman@yahoo.com>
2023-08-11 11:59:04 -0700

    u

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,25 +1,25 @@
 /**
- * _strcat - concatenate strings
+ * _strncat - concatenate up to n bytes
  * @dest: string to append to
  * @src: string to append
- * Return: result string
+ * @n: number of bytes
+ * Return: resultant string
  */
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
-	char *dest_start;
+	char *dest_start = dest;
 
-	dest_start = dest;
 	while (*dest != '\0')
 	{
 		dest++;
 	}
-
-	while (*src != '\0')
+	while (*src != '\0' && n > 0)
 	{
 		*dest = *src;
 		dest++;
 		src++;
+		n--;
 	}
 	*dest = '\0';
-	return (dest_start);
+return (dest_start);
 }
