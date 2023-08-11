e8f152ca10ee5e779c86eb9943b35bbc11462234
Pibo Shongwe <piboman@yahoo.com>
2023-08-11 12:53:03 -0700

    u

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,11 +1,40 @@
+#include "main.h"
 #include <stdio.h>
 #include <ctype.h>
+
+/**
+ * print_hexadecimal_with_padding - add padding to hexadecimal
+ * @value: input int
+ * Return: nothing
+ */
+void print_hexadecimal_with_padding(unsigned int value)
+{
+	int padding;
+	int shift; 
+	int i;
+	char digit;
+
+	shift = (padding - 1) * 4;
+	padding = 8;
+	for (i = 0; i < padding; i++)
+	{
+		digit = (value >> shift) & 0xF;
+		if (digit < 10)
+		{
+			_putchar('0' + digit);
+		}
+		else
+		{
+			_putchar('a' + digit - 10);
+		}
+		shift -= 4;
+	}
+}
 
 /**
  * print_buffer - Prints the content of a buffer.
  * @b: Pointer to the buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
- *
  * The function prints the content of size bytes of the buffer pointed by b.
  * The output prints 10 bytes per line. Each line starts with the position of
  * the first byte of the line in hexadecimal (8 chars), starting with 0.
@@ -23,38 +52,38 @@
  */
 void print_buffer(char *b, int size)
 {
-int i, j;
-if (size <= 0)
-{
-printf("\n");
-return;
-}
-for (i = 0; i < size; i += 10)
-{
-printf("%08x: ", i);
-for (j = 0; j < 10; j++)
-{
-if (i + j < size)
-printf("%02x", b[i + j] & 0xFF);
-else
-printf(" ");
-if (j % 2 == 1)
-printf(" ");
-}
-for (j = 0; j < 10; j++)
-{
-if (i + j < size)
-{
-if (isprint(b[i + j]))
-printf("%c", b[i + j]);
-else
-printf(".");
-}
-else
-{
-printf(" ");
-}
-}
-printf("\n");
-}
+	int i, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i += 10)
+	{
+		print_hexadecimal_with_padding(i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				print_hexadecimal_with_padding(b[i + j] & 0xFF);
+			else
+				_putchar(' ');
+			if (j % 2 == 1)
+				_putchar(' ');
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				if (isprint(b[i + j]))
+					print_hexadecimal_with_padding(b[i + j]);
+				else
+					_putchar('.';
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
