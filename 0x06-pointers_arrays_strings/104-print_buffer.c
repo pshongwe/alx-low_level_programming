469f79a42a643ef4018353f8bc6c066c54a36b84
Pibo Shongwe <piboman@yahoo.com>
2023-08-12 08:07:08 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -23,7 +23,7 @@ else
 printf("%02x", b[i * 10 + j]);
 
 if (j % 2 == 1)
-putchar(' ');
+_putchar(' ');
 }
 
 for (j = 0; j < 10; j++)
@@ -31,13 +31,13 @@ for (j = 0; j < 10; j++)
 if (j + i * 10 >= size)
 break;
 if (b[i * 10 + j] >= 32 && b[i * 10 + j] <= 126)
-putchar(b[i * 10 + j]);
+_putchar(b[i * 10 + j]);
 else
-putchar('.');
+_putchar('.');
 }
 
-putchar('\n');
+_putchar('\n');
 }
 if (size <= 0)
-putchar('\n');
+_putchar('\n');
 }
