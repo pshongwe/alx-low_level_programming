3541cebe80fe3e4390e3f9db42ac57cbab224831
Pibo Shongwe <piboman@yahoo.com>
2023-08-12 08:02:25 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,43 +1,43 @@
 #include "main.h"
 
 /**
- * print_buffer - Prints the content of a buffer.
- * @b: Pointer to the buffer to be printed.
- * @size: The number of bytes to be printed from the buffer.
- *
- * Return: Nothing.
- */
+* print_buffer - Prints the content of a buffer.
+* @b: Pointer to the buffer to be printed.
+* @size: The number of bytes to be printed from the buffer.
+*
+* Return: Nothing.
+*/
 void print_buffer(char *b, int size)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i <= (size - 1) / 10 && size; i++)
-    {
-        printf("%08x: ", i * 10);
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
 
-        for (j = 0; j < 10; j++)
-        {
-            if (j + i * 10 >= size)
-                printf("  ");
-            else
-                printf("%02x", b[i * 10 + j]);
+for (j = 0; j < 10; j++)
+{
+if (j + i * 10 >= size)
+printf("  ");
+else
+printf("%02x", b[i * 10 + j]);
 
-            if (j % 2 == 1)
-                putchar(' ');
-        }
+if (j % 2 == 1)
+putchar(' ');
+}
 
-        for (j = 0; j < 10; j++)
-        {
-            if (j + i * 10 >= size)
-                break;
-            if (b[i * 10 + j] >= 32 && b[i * 10 + j] <= 126)
-                putchar(b[i * 10 + j]);
-            else
-                putchar('.');
-        }
+for (j = 0; j < 10; j++)
+{
+if (j + i * 10 >= size)
+break;
+if (b[i * 10 + j] >= 32 && b[i * 10 + j] <= 126)
+putchar(b[i * 10 + j]);
+else
+putchar('.');
+}
 
-        putchar('\n');
-    }
-    if (size <= 0)
-        putchar('\n');
+putchar('\n');
+}
+if (size <= 0)
+putchar('\n');
 }
