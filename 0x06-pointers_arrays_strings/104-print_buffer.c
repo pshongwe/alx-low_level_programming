1a980f0f071f7a0e2f533cffda22d76bf88c3e75
Pibo Shongwe <piboman@yahoo.com>
2023-08-12 07:55:05 +0200

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,90 +1,102 @@
 #include "main.h"
-#include <stdio.h>
-#include <ctype.h>
+
+void print_hex_address(unsigned int address);
+void print_hex_byte(unsigned char byte);
+void print_printable_char(char c);
 
 /**
- * print_hexadecimal_with_padding - add padding to hexadecimal
- * @value: input int
- * Return: nothing
- */
-void print_hexadecimal_with_padding(unsigned int value)
+* print_buffer - Prints the content of a buffer.
+* @b: Pointer to the buffer to be printed.
+* @size: The number of bytes to be printed from the buffer.
+*/
+void print_buffer(char *b, int size)
 {
-	int padding;
-	int shift;
-	int i;
-	char digit;
+int i, j;
 
-	padding = 8;
-	shift = (padding - 1) * 4;
-	for (i = 0; i < padding; i++)
-	{
-		digit = (value >> shift) & 0xF;
-		if (digit < 10)
-		{
-			_putchar('0' + digit);
-		}
-		else
-		{
-			_putchar('a' + digit - 10);
-		}
-		shift -= 4;
-	}
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+print_hex_address(i * 10);
+
+for (j = 0; j < 10; j++)
+{
+if (j + i * 10 >= size)
+_putchar(' ');
+else
+print_hex_byte(b[i * 10 + j]);
+
+if (j % 2 == 1)
+_putchar(' ');
+}
+
+for (j = 0; j < 10; j++)
+{
+if (j + i * 10 >= size)
+break;
+print_printable_char(b[i * 10 + j]);
+}
+
+_putchar('\n');
+}
+if (size <= 0)
+_putchar('\n');
 }
 
 /**
- * print_buffer - Prints the content of a buffer.
- * @b: Pointer to the buffer to be printed.
- * @size: The number of bytes to be printed from the buffer.
- * The function prints the content of size bytes of the buffer pointed by b.
- * The output prints 10 bytes per line. Each line starts with the position of
- * the first byte of the line in hexadecimal (8 chars), starting with 0.
- * Each line shows the hexadecimal content (2 chars) of the buffer, 2 bytes at
- * a time, separated by a space.
- * Each line shows the content of the buffer. If the byte is a printable
- * character, print the letter, if not, print '.'.
- * Each line ends with a new line '\n'.
- * If size is 0 or less, the output should be a new line only '\n'.
- *
- * Example output:
- * 00000000: 48 65 6c 6c 6f 2c 20 77 6f 72 Hello, wor
- * 0000000a: 6c 64 21                      ld!
- * Return: nothing
- */
-void print_buffer(char *b, int size)
+* print_hex_address - Prints the hexadecimal representation of memory addresses.
+* @address: The memory address to print.
+*/
+void print_hex_address(unsigned int address)
 {
-	int i, j;
-	
-	if (size <= 0)
-	{
-		_putchar('\n');
-		return;
-	}
-	for (i = 0; i < size; i += 10)
-	{
-		print_hexadecimal_with_padding(i);
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-				print_hexadecimal_with_padding(b[i + j] & 0xFF);
-			else
-				_putchar(' ');
-			if (j % 2 == 1)
-				_putchar(' ');
-		}
-		for (j = 0; j < 10; j++)
-		{
-			if (i + j < size)
-			{
-				if (isprint(b[i + j]))
-					print_hexadecimal_with_padding(b[i + j]);
-				else
-					_putchar('.');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+int shift;
+unsigned int mask, hex_digit;
+
+_putchar('0');
+_putchar('x');
+
+for (shift = 28; shift >= 0; shift -= 4)
+{
+mask = 0xFU << shift;
+hex_digit = (address & mask) >> shift;
+
+if (hex_digit < 10)
+_putchar(hex_digit + '0');
+else
+_putchar(hex_digit - 10 + 'a');
+}
+}
+
+/**
+* print_hex_byte - Prints the hexadecimal representation of a byte.
+* @byte: The byte to print.
+*/
+void print_hex_byte(unsigned char byte)
+{
+unsigned char hex_digit;
+
+hex_digit = byte >> 4;
+hex_digit = hex_digit & 0x0F;
+
+if (hex_digit < 10)
+_putchar(hex_digit + '0');
+else
+_putchar(hex_digit - 10 + 'a');
+
+hex_digit = byte & 0x0F;
+
+if (hex_digit < 10)
+_putchar(hex_digit + '0');
+else
+_putchar(hex_digit - 10 + 'a');
+}
+
+/**
+* print_printable_char - Prints a printable character or '.' if not printable.
+* @c: The character to print.
+*/
+void print_printable_char(char c)
+{
+if (c >= 32 && c <= 126)
+_putchar(c);
+else
+_putchar('.');
 }
