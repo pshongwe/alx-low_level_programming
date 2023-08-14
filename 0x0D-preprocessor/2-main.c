2f10554f0c7f613829423a3e7771bab6c2450649
Pibo Shongwe <piboman@yahoo.com>
2023-08-14 12:49:49 -0700

    u

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char filename[] = __FILE__;
+	int i;
+
+	for (i = 0; filename[i] != '\0'; i++)
+	{
+		_putchar(filename[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
