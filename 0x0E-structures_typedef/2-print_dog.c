62d42569f320e671936b001040eee3d9a641f2aa
Pibo Shongwe <piboman@yahoo.com>
2023-08-14 21:35:36 -0700

    u

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,7 +4,7 @@
 
 /**
  * print_dog - print dog structure
- * @d: dog object 
+ * @d: dog object
  * Return: nothing
  */
 void print_dog(struct dog *d)
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		d->name = "(nil)"; 
+		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, d->owner);
