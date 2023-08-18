46ca3254ad39791942619c711ea21ac504602574
Pibo Shongwe <piboman@yahoo.com>
2023-08-18 13:39:46 -0700

    u

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,46 +1,52 @@
 #include <stdio.h>
-#include <stddef.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * print_all - Prints a variable number of
- * arguments based on the provided format.
+ * print_all - Prints a variable number of arguments
+ * based on the provided format.
  * @format: The format string specifying the types of arguments to be printed.
  * @...: A variable number of arguments to be printed based on the format.
  */
 void print_all(const char * const format, ...)
 {
-va_list args;
-const char *fmt;
+	va_list args;
+	int i = 0;
+	char c;
+	float f;
+	char *s;
 
-va_start(args, format);
-
-for (fmt = format; *fmt; fmt++)
-{
-if (*fmt == '%')
-{
-fmt++;
-switch (*fmt)
-{
-case 'd':
-printf("%d", va_arg(args, int));
-break;
-case 'f':
-printf("%f", va_arg(args, double));
-break;
-case 's':
-printf("%s", va_arg(args, char *));
-break;
-default:
-printf("%c", *fmt);
-break;
-}
-}
-else
-{
-printf("%c", *fmt);
-}
-}
-
+	va_start(args, format);
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+			default:
+				break;
+		}
+		if (format[i + 1] && (format[i] == 'c'
+		|| format[i] == 'i' || format[i] == 'f' || format[i] == 's'))
+		printf(", ");
+		i++;
+	}
 va_end(args);
+printf("\n");
 }
+
