a30f5e79aa7b91afee62d5065a60534468347456
Pibo Shongwe <piboman@yahoo.com>
2023-08-18 13:09:32 -0700

    u

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,13 +12,15 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 va_list args;
 unsigned int i;
+const char *str;
 
 va_start(args, n);
-
 for (i = 0; i < n; i++)
 {
-printf("%s", va_arg(args, const char *));
-if (i < n - 1)
+str = va_arg(args, const char *);
+if (str != NULL)
+printf("%s", str);
+if (i < n - 1 && separator != NULL)
 {
 printf("%s", separator);
 }
