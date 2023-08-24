f0cfc441aea93df7de5253368790f343969cafda
Pibo Shongwe <piboman@yahoo.com>
2023-08-24 16:42:20 -0700

    u

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,8 @@
 section .data
     hello db 'Hello, Holberton', 0
-    newline db 10, 0 ; Newline character '\n'
+    newline db 10, 0  ; Definition of newline as '\n'
+
+section .data
     format db '%s', 0
 
 section .text
@@ -11,9 +13,10 @@ main:
     push rbp
     mov rdi, format
     mov rsi, hello
-    call printf
+    call printf wrt ..plt  ; Use "call printf wrt ..plt" instead of "call printf"
     mov rdi, format
     mov rsi, newline
-    call printf
+    call printf wrt ..plt  ; Use "call printf wrt ..plt" instead of "call printf"
     pop rbp
     ret
+
