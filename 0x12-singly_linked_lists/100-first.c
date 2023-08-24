461fc7628c9a54a3275fef6a3c32917108acf302
Pibo Shongwe <piboman@yahoo.com>
2023-08-24 16:23:38 -0700

    u

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * before_main - Prints a specific message before
+ * the main function is executed.
+ * Return: nothing
+ */
 void __attribute__((constructor)) before_main()
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
 }
 
