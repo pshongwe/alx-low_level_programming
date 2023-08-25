d726e42245d25ca9c93c10ba4f0da61e5045380e
Pibo Shongwe <piboman@yahoo.com>
2023-08-24 17:13:09 -0700

    u

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,15 @@
-section .data
-    hello db 'Hello, Holberton', 0
-    newline db 10, 0  ; Definition of newline as '\n'
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+format: db "%s", 10, 0
 
-section .data
-    format db '%s', 0
-
-section .text
-    global main
-    extern printf
-
+	SECTION .text
+	global main
+	extern printf
 main:
-    sub rsp, 8  ; Align the stack
-    mov rdi, format
-    mov rsi, hello
-    call printf wrt ..plt
-    mov rdi, format
-    mov rsi, newline
-    call printf wrt ..plt
-    add rsp, 8  ; Restore the stack
-    ret
+	mov esi, msg
+	mov edi, format
+	mov eax, 0
+	call printf
 
+	mov eax, 0
+	ret
