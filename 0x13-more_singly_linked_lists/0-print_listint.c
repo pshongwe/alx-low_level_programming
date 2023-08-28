23f8dd9d5db4e5bda3d224a2af34bbfa2c913d74
Pibo Shongwe <piboman@yahoo.com>
2023-08-28 13:35:01 -0700

    u

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_listint - prints number of nodes
+ * @h: head node
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count;
+	const listint_t *current;
+
+	count = 0;
+	current  = h;
+	
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
