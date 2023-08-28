d1ade463ca5b0d8b75bdf7ac00723ee3518f6410
Pibo Shongwe <piboman@yahoo.com>
2023-08-28 14:27:47 -0700

    u

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t list.
+ * @head: Pointer to the head of the linked list to be freed.
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *current, *next_node;
+
+
+if (head == NULL)
+	return (NULL);
+current = *head;
+
+while (current != NULL)
+{
+next_node = current->next;
+free(current);
+current = next_node;
+}
+*head = NULL;
+}
+
