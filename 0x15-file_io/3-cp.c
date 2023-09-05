cde60dc6392af18be2e880daa9ffc9d4e02c21cc
Pibo Shongwe <piboman@yahoo.com>
2023-09-05 15:44:16 -0700

    u

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -60,12 +60,13 @@ error_exit(99, argv[2], 0);
 
 while ((bytes_read = read(fd_from, buffer, BUFSIZE)) > 0)
 {
-if (bytes_read == -1)
-error_exit(98, argv[1], 0);
 bytes_written = write(fd_to, buffer, bytes_read);
 if (bytes_written == -1)
 error_exit(99, argv[2], 0);
 }
+
+if (bytes_read == -1)
+error_exit(98, argv[1], 0);
 
 if (close(fd_from) == -1)
 error_exit(100, NULL, fd_from);
