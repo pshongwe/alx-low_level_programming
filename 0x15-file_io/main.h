18bfb9fb07deaec730210a01a273b981939afcc9
Pibo Shongwe <piboman@yahoo.com>
2023-10-02 20:19:22 +0200

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,4 +14,14 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+void print_magic(Elf64_Ehdr *header);
+void print_class(Elf64_Ehdr *header);
+void print_data(Elf64_Ehdr *header);
+void print_version(Elf64_Ehdr *header);
+void print_os_abi(Elf64_Ehdr *header);
+void print_abi_version(Elf64_Ehdr *header);
+void print_type(Elf64_Ehdr *header);
+void print_entry_point(Elf64_Ehdr *header);
+void print_elf_header_info(Elf64_Ehdr *header);
+
 #endif /* MAIN_H */
