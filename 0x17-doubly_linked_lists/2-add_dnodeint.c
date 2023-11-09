33032e0d162814021d19322adb399394c9194355
Pibo Shongwe <piboman@yahoo.com>
2023-11-09 15:11:56 -0800

    u

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,11 +16,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 	h = *head;
-	if (h != NULL)
-	{
-		while (h->prev != NULL)
-			h = h->prev;
-	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -28,6 +23,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = h;
+	if (h != NULL)
+		h->prev = new_node;
 	*head = new_node;
 	return (new_node);
 }
