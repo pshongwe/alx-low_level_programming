99ca5008d9360a1b30db4c38717ab423601a6e2c
Pibo Shongwe <piboman@yahoo.com>
2023-11-09 15:20:01 -0800

    u

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,6 +31,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (current->next)
 		current = current->next;
 	current->next = new_node;
-	new_node->prev = *head;
+	new_node->prev = current;
 	return (new_node);
 }
