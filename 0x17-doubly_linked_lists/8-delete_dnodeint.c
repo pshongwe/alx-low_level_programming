6802db532c30d8f77a57d61c339b1221574813f0
Pibo Shongwe <piboman@yahoo.com>
2023-11-13 11:02:02 -0800

    u

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -35,7 +35,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				return (-1);
 			current->next = temp->next;
 			if (temp->next != NULL)
-				temp->next-prev = current;
+				temp->next->prev = current;
 			free(temp);
 			return (1);
 		}
