3f63bacee1f2d22541a01704a7c26785004c891d
Pibo Shongwe <piboman@yahoo.com>
2023-11-13 21:34:52 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,51 +12,51 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *current, *new_node;
-    unsigned int i;
+unsigned int i;
 
-    if (h == NULL)
-        return (NULL);
+if (h == NULL)
+return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
-    new_node->prev = NULL;
+new_node->n = n;
+new_node->next = NULL;
+new_node->prev = NULL;
 
-    if (idx == 0)
-    {
-        new_node = add_dnodeint(h, n);
-        return (new_node);
-    }
+if (idx == 0)
+{
+new_node = add_dnodeint(h, n);
+return (new_node);
+}
 
-    current = *h;
-    i = 0;
+current = *h;
+i = 0;
 
-    while (current != NULL)
-    {
-        if (i == idx - 1)
-        {
-            if (current->next == NULL)
-            {
-                new_node->prev = current;
-                current->next = new_node;
-                return (new_node);
-            }
-            new_node->next = current->next;
-            new_node->prev = current;
+while (current != NULL)
+{
+if (i == idx - 1)
+{
+if (current->next == NULL)
+{
+new_node->prev = current;
+current->next = new_node;
+return (new_node);
+}
+new_node->next = current->next;
+new_node->prev = current;
 
-            if (current->next != NULL)
-                current->next->prev = new_node;
+if (current->next != NULL)
+current->next->prev = new_node;
 
-            current->next = new_node;
-            return (new_node);
-        }
+current->next = new_node;
+return (new_node);
+}
 
-        current = current->next;
-        i++;
-    }
-    free(new_node);
-    return (NULL);
+current = current->next;
+i++;
+}
+free(new_node);
+return (NULL);
 }
