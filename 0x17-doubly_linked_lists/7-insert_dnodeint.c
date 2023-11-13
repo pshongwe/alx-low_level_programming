0a5778ab0b5733e1496788daca52875dfda9d6ba
Pibo Shongwe <piboman@yahoo.com>
2023-11-13 11:34:17 -0800

    u

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,40 +12,51 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 dlistint_t *current, *new_node;
-unsigned int i;
+    unsigned int i;
 
-if (h == NULL)
-return (NULL);
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
-new_node->next = NULL;
-new_node->prev = NULL;
-if (idx == 0)
-{
-new_node = add_dnodeint(h, n);
-return (new_node);
-}
-current = *h;
-i = 0;
-while (current != NULL)
-{
-if (i == idx)
-{
-if (current->next == NULL)
-	new_node = add_dnodeint_end(h, n);
-else if (new_node != NULL)
-{
-new_node->next = current->next;
-new_node->prev = current;
-current->next->prev = new_node;
-current->next = new_node;
-}
-break;
-}
-current = current->next;
-i++;
-}
-return (new_node);
+    if (h == NULL)
+        return (NULL);
+
+    new_node = malloc(sizeof(dlistint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = n;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+
+    if (idx == 0)
+    {
+        new_node = add_dnodeint(h, n);
+        return (new_node);
+    }
+
+    current = *h;
+    i = 0;
+
+    while (current != NULL)
+    {
+        if (i == idx - 1)
+        {
+            if (current->next == NULL)
+            {
+                new_node->prev = current;
+                current->next = new_node;
+                return (new_node);
+            }
+            new_node->next = current->next;
+            new_node->prev = current;
+
+            if (current->next != NULL)
+                current->next->prev = new_node;
+
+            current->next = new_node;
+            return (new_node);
+        }
+
+        current = current->next;
+        i++;
+    }
+    free(new_node);
+    return (NULL);
 }
