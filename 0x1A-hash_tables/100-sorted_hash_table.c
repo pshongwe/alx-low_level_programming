cf999c210968e3dea699e9354bf2ed005c32eb67
Pibo Shongwe <piboman@yahoo.com>
2023-12-02 05:03:17 +0200

    Update 100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -182,36 +182,6 @@ ht->stail = NULL;
 return (ht);
 }
 
-int shash_table_set(shash_table_t *ht, const char *key, const char *value)
-{
-shash_node_t *new_node, *temp, *prev_node;
-unsigned long int index;
-
-if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
-return 0;
-
-index = key_index((const unsigned char *)key, ht->size);
-temp = ht->array[index];
-while (temp) {
-if (strcmp(temp->key, key) == 0) {
-free(temp->value);
-temp->value = strdup(value);
-return 1;
-}
-temp = temp->next;
-}
-
-new_node = malloc(sizeof(shash_node_t));
-if (new_node == NULL)
-return (0);
-new_node->key = strdup(key);
-new_node->value = strdup(value);
-if (new_node->key == NULL || new_node->value == NULL)
-{
-free(new_node);
-return (0);
-}
-
 new_node->next = ht->array[index];
 ht->array[index] = new_node;
 temp = ht->shead;
